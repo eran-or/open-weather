310a36fe53c7517a95f2d69593628c24f21d81ff
eranor <e.or1976@gmail.com>
2019-01-10 10:59:50 +0200

    fix routing for demo

--- client/src/routers/AppRouter.js
+++ client/src/routers/AppRouter.js
@@ -4,8 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from '../components/Home'
 
 const AppRouter = (props) => (
-  // <Router basename="/comment-feed/">
-  <Router>
+  <Router basename="/open-weather/">
     <div>
       <Switch>
         <Route path="/" component={Home} exact={true} />
