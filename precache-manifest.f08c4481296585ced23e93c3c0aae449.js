b4bb64b191ed11a3b84ef1d60338a6dac337b57c
eranor <e.or1976@gmail.com>
2019-01-10 11:33:57 +0200

    Updates

---
+++ precache-manifest.f08c4481296585ced23e93c3c0aae449.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b677991086fe59dcd76c",
+    "url": "/open-weather/static/js/runtime~main.b6779910.js"
+  },
+  {
+    "revision": "4ba90fe6a41eff692fef",
+    "url": "/open-weather/static/js/main.4ba90fe6.chunk.js"
+  },
+  {
+    "revision": "62141f7f6ba3aece95c5",
+    "url": "/open-weather/static/js/1.62141f7f.chunk.js"
+  },
+  {
+    "revision": "4ba90fe6a41eff692fef",
+    "url": "/open-weather/static/css/main.8afe7bfc.chunk.css"
+  },
+  {
+    "revision": "62141f7f6ba3aece95c5",
+    "url": "/open-weather/static/css/1.b8a760f9.chunk.css"
+  },
+  {
+    "revision": "f8ed81cfba88d86fab5982172cb54531",
+    "url": "/open-weather/index.html"
+  }
+];
